47c49007061455db51dc8a24deb09722e76d0f66
Jaros <93591284+wjaros@users.noreply.github.com>
2023-04-04 22:36:53 +0200

    first draw

---
+++ cv.js
@@ -0,0 +1,19 @@
+const candidate = {
+    firstName: 'Wojciech',
+    lastName: 'Jaros',
+    role: 'Test Automation Engineer',
+    address = {
+        country: 'Poland',
+        city: 'Warsaw',
+        email: 'w.jaros@homtmail.com',
+        mobile: '+48 889497238'
+    }
+}
+
+function experience(){
+    const aleno = {
+        timeline: '01.05.2022 - present',
+        role: 'Test Automation Engineer',
+        responibilites
+    }
+}
